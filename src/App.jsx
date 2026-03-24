7d6cc90d8aeb3780114aed00f162ab516fdf1103
CjL95 <cjlong95@gmail.com>
2026-03-23 21:00:25 -0400

    basic rough design implemented

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,31 @@ function App() {
   return (
     <>
       <Header />
-      <main className="container main">
+      <main className="container-fluid main">
         <section className="hero row">
-          <figure className="profilePic col-6">
+          <figure className="profilePic col-12 col-lg-6">
             <img src="./src/assets/250-250.png" />
           </figure>
-          <div className="heroTxt col-6">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quam nobis perferendis amet, ullam mollitia sit perspiciatis? Quibusdam eos rem rerum nam iste reprehenderit aliquam, totam eius odio quisquam enim!</h1>
+          <div className="heroTxt col-12 col-lg-6">
+            <span>
+              <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quam nobis perferendis amet, ullam mollitia sit perspiciatis? Quibusdam eos rem rerum nam iste reprehenderit aliquam, totam eius odio quisquam enim!</h1>
+            </span>
           </div>
         </section>
         <section className="handwrittenCaption">
           <span className="handwrittenCaption"></span>
+        </section>
+        <section className="about row">
+          
+        </section>
+        <section className="projects row">
+          
+        </section>
+        <section className="contact row">
+          
+        </section>
+        <section className="footer row">
+          
         </section>
       </main>
       <Footer />
